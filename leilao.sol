384ff7ed6a5532264ab17b9d2aad72fce88e5296
Lucas Moreira Minchillo <64343934+minchillo4@users.noreply.github.com>
2021-10-29 03:17:01 -0300

    Update leilao.sol

--- leilao.sol
+++ leilao.sol
@@ -2,23 +2,23 @@ pragma solidity 0.8.9;
 
 contract leilao {
     // Parametros do leilao;
-    address payable public beneficiario;
-    uint public tempoEncerramentoLeilao;
+    address payable public beneficiario;    // Carteira do beneficiário, que recebera o valor do leilão
+    uint public tempoEncerramentoLeilao;    // Data do encerramento do leilão em UTC em epoch time
     
     // Estado atual do leilao;
     
-    address public endMaiorOferta;
-    uint public valorMaiorOferta;
+    address public endMaiorOferta;          // Variavel armazena o endereço com o maior valor de valorMaiorOferta
+    uint public valorMaiorOferta;          //  Varivavel armazena o valor da maior oferta
     
-    mapping(address => uint) public ExtornoPendente;
+    mapping(address => uint) public ExtornoPendente; // Mapeamento armazena que conecta endereço dos ofertantes, aos seus valores, posteriormente, o mapeamento vai ser utilizado para retirar os valores de ofertas não vencedoras 
     
-    bool terminou = false;
+    bool terminou = false;  // variavel que  armazena o estado do leilao, aberto ou fechado
     
-    event aumentoMaiorOferta(address _participante, uint _valor);
+    event aumentoMaiorOferta(address _participante, uint _valor); 
     event leilaoEncerrado(address _vencedor, uint _valor);  
     
     //
-    constructor (uint tempoLeilao, address payable _participante) {
+    constructor (uint tempoLeilao, address payable _participante) {  // construtor  
         beneficiario = _participante;
         tempoEncerramentoLeilao = block.timestamp + tempoLeilao;
     }
@@ -26,29 +26,23 @@ contract leilao {
     function ofertar() public payable {
         require(block.timestamp <= tempoEncerramentoLeilao, "Desculpe, o leilao esta encerrado");
         require(msg.value > valorMaiorOferta && msg.value != 0, "Desculpe, sua oferta  nao e a maior.");
-        
-        ExtornoPendente[endMaiorOferta] += valorMaiorOferta;
         valorMaiorOferta = msg.value;
         endMaiorOferta = msg.sender;
+        ExtornoPendente[endMaiorOferta] += valorMaiorOferta;
         emit aumentoMaiorOferta (msg.sender, msg.value);
     }
         
     
     
-    function retirar() public returns (bool) {
+    function retirar() public payable  {
         uint amount = ExtornoPendente[msg.sender];
         require(amount>0);
-            ExtornoPendente[msg.sender] = 0;
-            
-            if(!payable(msg.sender).send(amount)){
-                ExtornoPendente[msg.sender] = amount;
-                return false;
-            }
-            return true;
+        payable(msg.sender).transfer(amount);
+         ExtornoPendente[msg.sender] = 0;
     }
     
     function leilaoEncerrou() public {
-        require(terminou = true && block.timestamp > tempoEncerramentoLeilao, "Leilao em aberto");
+        require(terminou = false && block.timestamp > tempoEncerramentoLeilao, "Leilao em aberto");
         terminou = true;
         emit leilaoEncerrado(endMaiorOferta, valorMaiorOferta);
         beneficiario.transfer(valorMaiorOferta);
@@ -58,6 +52,3 @@ contract leilao {
     }
 
         
-
-    
-}
